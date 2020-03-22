db84bc803519f839e62c2462b310a92510550d90
BRabie <32295818+BRabie@users.noreply.github.com>
2020-03-22 12:09:07 +0100

    Update App.js

--- App.js
+++ App.js
@@ -9,32 +9,20 @@ import FavReducer from './Context/FavReducer.js'
 
 export default function App() {
 
-	const initialState = []
-
-	const [state, dispach] = useReducer(FavReducer, initialState)
+	const [state, dispach] = useReducer(FavReducer, [])
 
 	const toggleFav = (film) => {
-		dispach({
-			type: 'TOGGLE_FAV',
-			payload: film
-		})
+		dispach({ type: 'TOGGLE_FAV',	payload: film	})
 	}
 
 	const setFav = (array) => {
-		console.log('setFav')
-		dispach({
-			type: 'INIT_FAV',
-			payload: array
-		})
+		dispach({	type: 'INIT_FAV',	payload: array })
 	}
 
 	useEffect( () => {
-		console.log('useEffect')
 		AsyncStorage.getItem('favorite')
 		.then( (result) => { if (result) setFav(JSON.parse(result)) } )
 	}, [])
-
-	//AsyncStorage.getItem('favorite').then( (result) => console.log('storage :', result) )
 
 	return (
 		<FavContext.Provider value={{films: state, toggleFav}}>
